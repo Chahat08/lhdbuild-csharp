57d4ca41ba0816b791058f8af0ded232e10dba0a
Chahat Kalsi <chahat.ck88@gmail.com>
2021-01-11 20:41:10 +0530

    first program

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+	    Console.WriteLine("Thank you MLH for helping me get started with a new language!!");
+        }
+    }
+}
